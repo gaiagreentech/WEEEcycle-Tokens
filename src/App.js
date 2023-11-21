7b68c5b7f17a4f7655889133e3d0382f751a577b
Marcelo <marcelo.chiarella@gmail.com>
2023-11-21 16:17:12 -0300

    authenticate() not working even with .env with PK created, due to the prefix react_app_ (required by react) not being expected by the FGStorage dependency. To be corrected in the next commit.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import { FGStorage } from '@co2-storage/js-api';
+import { FGStorage, Auth } from '@co2-storage/js-api';
 
 const ipfsNodeAddr = "/dns4/web2.co2.storage/tcp/5002/https"
 const fgApiUrl = "https://web2.co2.storage"
+
+const authType = "pk"
+const auth = new Auth(authType)
 
 const fgStorage = new FGStorage({
   authType: "metamask", 
@@ -10,6 +13,24 @@ const fgStorage = new FGStorage({
   ipfsNodeAddr: ipfsNodeAddr, 
   fgApiHost: fgApiUrl
 })
+
+async function authenticate(){
+  /**
+   * Authenticate with a private key
+   */
+
+  console.log('authenticate')
+  let authResponse = await auth.authenticate()
+
+  if(authResponse.error != null) {
+      console.error(authResponse.error)
+      await new Promise(reject => setTimeout(reject, 300));
+  }
+
+  console.dir(authResponse.result, {depth: null})
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
+}
 
 async function searchTemplates(){
   console.log('searchTemplates')
@@ -33,6 +54,7 @@ function App() {
         <p>
           Filecoin CO2.Storage examples.
         </p>        
+        <button onClick={authenticate}>Authenticate</button>
         <button onClick={searchTemplates}>searchTemplates</button>
       </header>
     </div>
