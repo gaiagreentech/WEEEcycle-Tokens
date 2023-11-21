aff308b8c3f54b9d2cad7df2f523acc32322c6d0
Marcelo <marcelo.chiarella@gmail.com>
2023-11-21 15:59:19 -0300

    fgStorage.searchTemplates working.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import { FGStorage } from '@co2-storage/js-api';
 
-const authType = "metamask"
-const ipfsNodeType = "browser"
 const ipfsNodeAddr = "/dns4/web2.co2.storage/tcp/5002/https"
 const fgApiUrl = "https://web2.co2.storage"
 
-const fgStorage = new FGStorage({authType: authType, ipfsNodeType: ipfsNodeType, ipfsNodeAddr: ipfsNodeAddr, fgApiHost: fgApiUrl})
+const fgStorage = new FGStorage({
+  authType: "metamask", 
+  ipfsNodeType: "browser", 
+  ipfsNodeAddr: ipfsNodeAddr, 
+  fgApiHost: fgApiUrl
+})
+
+async function searchTemplates(){
+  console.log('searchTemplates')
+  /**
+   * Search templates
+   * parameters: (chainName, phrases, cid, name, base, account, offset, limit, sortBy, sortDir)
+   * default data_chain: 'sandbox', phrases: null, cid: null, name: null, base: null, account: null, offset: 0, limit: 10
+   */
+  
+let searchTemplatesResponse = await fgStorage.searchTemplates('sandbox')    // ('SP Audits', 'Water')
+  if(searchTemplatesResponse.error != null) {
+      console.error(searchTemplatesResponse.error)
+  }
+  console.log(searchTemplatesResponse.result)
+}
 
 function App() {
   return (
@@ -14,7 +32,8 @@ function App() {
       <header className="App-header">
         <p>
           Filecoin CO2.Storage examples.
-        </p>
+        </p>        
+        <button onClick={searchTemplates}>searchTemplates</button>
       </header>
     </div>
   );
