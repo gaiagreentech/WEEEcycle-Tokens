b899bd734d9771b6c5222be86921f12c5fd6a6f5
Marcelo <marcelo.chiarella@gmail.com>
2023-11-21 09:08:43 -0300

    After referencing FGStorage: ERROR in ./node_modules/micro-ftch/index.js BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default. Same for https, zlib and url.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { FGStorage } from '@co2-storage/js-api';
+
+const authType = "metamask"
+const ipfsNodeType = "browser"
+const ipfsNodeAddr = "/dns4/web2.co2.storage/tcp/5002/https"
+const fgApiUrl = "https://web2.co2.storage"
+
+const fgStorage = new FGStorage({authType: authType, ipfsNodeType: ipfsNodeType, ipfsNodeAddr: ipfsNodeAddr, fgApiHost: fgApiUrl})
 
 function App() {
   return (
